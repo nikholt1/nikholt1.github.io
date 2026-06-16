7f1a58a1976b3cb4db5177c8eb34b3a4fbdc7bd3
nikholt1 <niklas1997@live.dk>
2026-06-16 15:45:00 +0200

    made rules

---
+++ gameAssets/js/app.js
@@ -0,0 +1,12 @@
+import {initRules} from "./rules.js";
+
+
+document.addEventListener("DOMContentLoaded", () => initApp());
+
+
+function initApp() {
+    console.log("DOM Loaded");
+
+
+    // initRules()
+}
---
+++ gameAssets/js/rules.js
@@ -0,0 +1,80 @@
+
+
+
+
+export function initRules() {
+    const ruleContainer = document.getElementById("rulesContainer");
+    ruleContainer.innerHTML = `
+        <div id="ruleCard" class="ruleCard">
+        <strong class="number">1.</strong>
+        <div class="ruleTextPart">
+            <h3 class="header">Start Rule</h3>
+            <p class="ruleText">
+
+                All players of the game must define their word (words combined).
+                Once the words are defined, everyone must vote on the best word.
+                The person with the best word gets 1 point.
+            </p>
+
+        </div>
+
+    </div>
+    <div id="ruleCard2" class="ruleCard">
+        <strong class="number">2.</strong>
+
+        <p class="ruleText">
+            All players of the game must define their word (words combined).
+            Once the words are defined, everyone must vote on the best word.
+            The person with the best word gets 1 point.
+        </p>
+    </div>
+    <div id="ruleCard3" class="ruleCard">
+        <strong class="number">3.</strong>
+        <div class="ruleTextPart">
+            <h3 class="header">Start Rule</h3>
+            <p class="ruleText">
+
+                All players of the game must define their word (words combined).
+                Once the words are defined, everyone must vote on the best word.
+                The person with the best word gets 1 point.
+            </p>
+
+        </div>
+
+    </div>
+    <div id="ruleCard4" class="ruleCard">
+        <strong class="number">4.</strong>
+
+        <p class="ruleText">
+            All players of the game must define their word (words combined).
+            Once the words are defined, everyone must vote on the best word.
+            The person with the best word gets 1 point.
+        </p>
+    </div>
+    <div id="ruleCard5" class="ruleCard">
+        <strong class="number">5.</strong>
+        <div class="ruleTextPart">
+            <h3 class="header">Start Rule</h3>
+            <p class="ruleText">
+
+                All players of the game must define their word (words combined).
+                Once the words are defined, everyone must vote on the best word.
+                The person with the best word gets 1 point.
+            </p>
+
+        </div>
+
+    </div>
+    <div id="ruleCard6" class="ruleCard">
+        <strong class="number">6.</strong>
+
+        <p class="ruleText">
+            All players of the game must define their word (words combined).
+            Once the words are defined, everyone must vote on the best word.
+            The person with the best word gets 1 point.
+        </p>
+    </div>
+    
+    
+    `;
+}
